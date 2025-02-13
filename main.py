c26f2d2844bf20a1de5244cf484c642a906fa8a0
IMPERIAL-E <denzeljako@gmail.com>
2025-02-13 00:08:48 -0400

    nuevas correcciones

--- main.py
+++ main.py
@@ -11,42 +11,64 @@ pygame.init()
 ventana = pygame.display.set_mode((constantes_del_juego.ANCHO_VENTANA, constantes_del_juego.ALTO_VENTANA))
 pygame.display.set_caption("MIND FEASTABLES") # Nombre del juego.
 
-Reloj = pygame.time.Clock() # FPS del juego
+# definir las variables de movimiento del jugador
+mover_arriba = False
+mover_abajo = False
+mover_izquierda = False
+mover_derecha = False
 
+
+reloj = pygame.time.Clock() # Crotrolar los frame rate
 # Bucle principal del juego
 run = True
 while run == True:
-    Reloj.tick(constantes_del_juego.FPS) # FPS del juego
+    reloj.tick(constantes_del_juego.FPS)  # Controla la velocidad del juego
+
+    # Calcular el movimiento del jugador
+    delta_x = 0
+    delta_y = 0
+
+    if mover_derecha == True:
+            delta_x = constantes_del_juego.VELOCIDAD
+
+    if mover_izquierda == True:
+            delta_x = constantes_del_juego.VELOCIDAD
+
+    if mover_arriba == True:
+            delta_y = constantes_del_juego.VELOCIDAD
+
+    if mover_abajo == True:
+            delta_y = constantes_del_juego.VELOCIDAD
+
+    # Mover al jugador
+    jugador_princ.movimiento(delta_x, delta_y)
     ventana.fill(constantes_del_juego.COLOR_BG)  # Limpia la pantalla con color    
     # Eventos del juego
+    jugador_princ.dibujar(ventana)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
-
-            # Eventos del teclado   
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT: # Si se presiona la tecla izquierda
-                jugador_princ.forma.move_ip(-5, 0)
-            if event.key == pygame.K_RIGHT: # Si se presiona la tecla derecha
-                jugador_princ.forma.move_ip(5, 0)
-            if event.key == pygame.K_UP: # Si se presiona la tecla arriba
-                jugador_princ.forma.move_ip(0, -5)
-            if event.key == pygame.K_DOWN: # Si se presiona la tecla de abajo
-                jugador_princ.forma.move_ip(0, 5)
-                
-        if event.type == pygame.KEYUP: # Nuevo evento para letras sueltas
-            if event.key == pygame.K_a: # Si se presiona la tecla izquierda
-                jugador_princ.forma.move_ip(-5, 0)
-            if event.key == pygame.K_d: # Si se presiona la tecla derecha
-                jugador_princ.forma.move_ip(5, 0)
-            if event.key == pygame.K_w: # Si se presiona la tecla arriba
-                jugador_princ.forma.move_ip(0, -5)
-            if event.key == pygame.K_s: # Si se presiona la tecla de abajo
-                jugador_princ.forma.move_ip(0, 5)
-
-    # Dibujar el personaje en la ventana
-    jugador_princ.dibujar(ventana) # Dibujar el personaje en la ventana
-    pygame.display.update()  # Actualiza la pantalla
+    pygame.display.update()  # Actualiza la pantalla                    
+      
+                                # Eventos del teclado   
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            mover_izquierda = True
+        if event.key == pygame.K_RIGHT:
+            mover_derecha = True    
+        if event.key == pygame.K_UP:
+            mover_arriba = True
+        if event.key == pygame.K_DOWN:
+            mover_abajo = True
+    # Para cuando se selte la tecla
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_LEFT:
+                mover_izquierda = False
+        if event.key == pygame.K_RIGHT:
+                mover_derecha = False    
+        if event.key == pygame.K_UP:
+                mover_arriba = False
+        if event.key == pygame.K_DOWN:
+                mover_abajo = False
     
 pygame.quit()
--- personaje.py
+++ personaje.py
@@ -6,5 +6,11 @@ class Personaje():
         self.forma = pygame.Rect(0, 0, constantes_del_juego.ANCHO_PERSONAJE, constantes_del_juego.ALTO_PERSONAJE)
         self.forma.center = (x,y)
 
+    def movimiento(self, delta_x, delta_y):
+        self.forma.x = self.forma.x + delta_x
+        self.forma.y = self.forma.y + delta_y
+
     def dibujar(self, interfaz):
         pygame.draw.rect(interfaz, constantes_del_juego.COLOR_PERSONAJE, self.forma)
+
+         
